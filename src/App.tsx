674774051e635901671971f07c71d97bde5ee012
Kbotei <eclipcenet@gmail.com>
2025-08-18 21:32:17 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import {
-  Routes,
-  Route,
-  Outlet,
-  NavLink,
-  type NavLinkRenderProps,
-} from "react-router";
+import { Routes, Route, Outlet, NavLink } from "react-router";
 import { Home } from "./pages/home";
 import Scholarship from "./pages/applications/scholarship";
 
@@ -25,9 +19,6 @@ const App = () => {
 export default App;
 
 const Layout = () => {
-  const navClasses = ({ isActive }: NavLinkRenderProps) =>
-    "nav-link" + (isActive ? " active" : "");
-
   return (
     <>
       <div className="container px-4">
@@ -55,12 +46,12 @@ const Layout = () => {
                 >
                   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                      <NavLink to="/" className={navClasses}>
+                      <NavLink to="/" className="nav-link">
                         Home
                       </NavLink>
                     </li>
                     <li className="nav-item">
-                      <NavLink to="/scholarship" className={navClasses}>
+                      <NavLink to="/scholarship" className="nav-link">
                         Scholarship
                       </NavLink>
                     </li>
